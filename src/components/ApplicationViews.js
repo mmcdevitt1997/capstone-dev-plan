cea481cb3c3aed6a672ae043ff7587e0fa55892e
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-08-28 09:54:02 -0500

    still working on log in fix

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,81 +1,74 @@
 // eslint-disable-next-line
-import { Route, BrowserRouter as Router , withRouter, Redirect  } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  withRouter,
+  Redirect
+} from "react-router-dom";
 import React, { Component } from "react";
 import Project from "./projects/Project";
 import Task from "./tasks/Task";
-import ProjectHandler from "./apiHandler/ProjectHandler"
-import UserHandler from "./apiHandler/UserHandler"
-import TaskHandler from "./apiHandler/TaskHandler"
-import PhaseHandler from "./apiHandler/PhaseHandler"
-import getReposGithub from "./githubAPI/getReposGithub"
-import TaskEditCard from "./tasks/TaskEditCard"
-import SubTaskHandler from "./apiHandler/SubTaskHandler"
-import Ticket from "./projects/ticket-page /Ticket"
-import ProjectEdit from "./projects/ProjectEdit"
-
-
+import ProjectHandler from "./apiHandler/ProjectHandler";
+import UserHandler from "./apiHandler/UserHandler";
+import TaskHandler from "./apiHandler/TaskHandler";
+import PhaseHandler from "./apiHandler/PhaseHandler";
+import getReposGithub from "./githubAPI/getReposGithub";
+import TaskEditCard from "./tasks/TaskEditCard";
+import SubTaskHandler from "./apiHandler/SubTaskHandler";
+import Ticket from "./projects/ticket-page /Ticket";
+import ProjectEdit from "./projects/ProjectEdit";
 
 class ApplicationViews extends Component {
   state = {
     users: [],
     projects: [],
-    phases:[],
+    phases: [],
     tasks: [],
     teams: [],
     teamUsers: [],
     gitRepos: [],
-    subTask: []
-
-  };
-  componentDidMount()  {
-       UserHandler.getAll()
-      .then(users => this.setState({ users: users }))
-      .then(() =>  ProjectHandler.getAll())
-      .then (projects => {
-        this.setState({projects: projects})
-      })
-       .then(() => TaskHandler.getAll())
-       .then (tasks => {
-        this.setState({tasks:tasks})
-      })
-      .then(() => PhaseHandler.getAll())
-       .then (phases => {
-        this.setState({phases:phases})
-      })
-      .then(() => getReposGithub.getRepos())
-      .then (gitRepos => {
-       this.setState({gitRepos:gitRepos})
-     })
-     .then(() => SubTaskHandler.getAll())
-      .then (subTasks => {
-       this.setState({subTasks:subTasks})
-     })
+    subTask: [],
+    isLoaded: false
   }
 
-  updateProject = editProject=>
+  componentDidMount() {
+    let newState = {isLoaded: true}
+    UserHandler.getAll()
+      .then(users => newState.users = users)
+      .then(() => ProjectHandler.getAll())
+      .then(projects => newState.projects = projects)
+      .then(() => TaskHandler.getAll())
+      .then(tasks => newState.tasks = tasks)
+      .then(() => PhaseHandler.getAll())
+      .then(phases => newState.phases = phases)
+      .then(() => getReposGithub.getRepos())
+      .then(gitRepos => newState.gitRepos = gitRepos)
+      .then(() => SubTaskHandler.getAll())
+      .then(subTasks => newState.subTask = subTasks)
+      .then(() => this.setState(newState));
+  }
+
+  updateProject = editProject =>
     ProjectHandler.put(editProject)
       .then(() => ProjectHandler.getAll())
       .then(projects =>
         this.setState({
-           projects: projects
+          projects: projects
         })
-      )
+      );
 
-
-  addTask = (task) =>
+  addTask = task =>
     TaskHandler.post(task)
       .then(() => TaskHandler.getAll())
-      .then(tasks =>
-        this.setState({ tasks:tasks })
-      )
+      .then(tasks => this.setState({ tasks: tasks }));
 
   deleteTask = id => {
     TaskHandler.delete(id)
-    .then(() => TaskHandler.getAll())
-    .then (tasks => {
-      this.setState({tasks:tasks})
-    })
-  }
+      .then(() => TaskHandler.getAll())
+      .then(tasks => {
+        this.setState({ tasks: tasks });
+      });
+  };
 
   updateTask = task =>
     TaskHandler.put(task)
@@ -86,37 +79,32 @@ class ApplicationViews extends Component {
         });
       });
 
-
   addProject = project =>
     ProjectHandler.post(project)
       .then(() => ProjectHandler.getAll())
-      .then(projects =>
-        this.setState({ projects: projects })
-      )
-
+      .then(projects => this.setState({ projects: projects }));
 
   deleteProject = id => {
     ProjectHandler.delete(id)
-    .then(() => ProjectHandler.getAll())
-    .then (projects => {
-      this.setState({projects:projects})
-    })
-  }
+      .then(() => ProjectHandler.getAll())
+      .then(projects => {
+        this.setState({ projects: projects });
+      });
+  };
   patchProject = (edit, id) => {
     ProjectHandler.patch(edit, id)
-    .then(() => ProjectHandler.getAll())
-    .then (projects => {
-      this.setState({projects:projects})
-    })
-  }
-  patchTask= (edit, id) => {
+      .then(() => ProjectHandler.getAll())
+      .then(projects => {
+        this.setState({ projects: projects });
+      });
+  };
+  patchTask = (edit, id) => {
     TaskHandler.patch(edit, id)
-    .then(() => TaskHandler.getAll())
-    .then (tasks => {
-      this.setState({tasks:tasks})
-    })
-  }
-
+      .then(() => TaskHandler.getAll())
+      .then(tasks => {
+        this.setState({ tasks: tasks });
+      });
+  };
 
   isAuthenticated = () => sessionStorage.getItem("userId") !== null;
 
@@ -124,15 +112,24 @@ class ApplicationViews extends Component {
     return (
       <React.Fragment>
         <div>
-
           <Route
-           exact
+            exact
             path="/"
             render={props => {
               if (this.isAuthenticated()) {
-            return <Task {...props} tasks={this.state.tasks} projects={this.state.projects} phases={this.state.phases} deleteTask={this.deleteTask}  updateTask ={this.updateTask}  addTask={this.addTask} />
-          } else {
-            return <Redirect to="/login" />;
+                return (
+                  <Task
+                    {...props}
+                    tasks={this.state.tasks}
+                    projects={this.state.projects}
+                    phases={this.state.phases}
+                    deleteTask={this.deleteTask}
+                    updateTask={this.updateTask}
+                    addTask={this.addTask}
+                  />
+                );
+              } else {
+                return <Redirect to="/login" />;
               }
             }}
           />
@@ -141,10 +138,20 @@ class ApplicationViews extends Component {
             path="/projects"
             render={props => {
               if (this.isAuthenticated()) {
-              return <Project {...props} projects={this.state.projects} gitRepos={this.state.gitRepos} deleteProject={this.deleteProject} updateProject={this.updateProject} phases={this.state.phases} addProject={this.addProject} />
-            } else {
-              return <Redirect to="/login" />;
-                }
+                return (
+                  <Project
+                    {...props}
+                    projects={this.state.projects}
+                    gitRepos={this.state.gitRepos}
+                    deleteProject={this.deleteProject}
+                    updateProject={this.updateProject}
+                    phases={this.state.phases}
+                    addProject={this.addProject}
+                  />
+                );
+              } else {
+                return <Redirect to="/login" />;
+              }
             }}
           />
           <Route
@@ -152,60 +159,92 @@ class ApplicationViews extends Component {
             path="/projects/:id/tickets"
             render={props => {
               if (this.isAuthenticated()) {
-              return <Ticket {...props} projects={this.state.projects} tasks={this.state.tasks} phases={this.state.phases} addTask={this.addTask}/>
-            } else {
-              return <Redirect to="/login" />;
-                }
+                return (
+                  <Ticket
+                    {...props}
+                    projects={this.state.projects}
+                    tasks={this.state.tasks}
+                    phases={this.state.phases}
+                    addTask={this.addTask}
+                  />
+                );
+              } else {
+                return <Redirect to="/login" />;
+              }
             }}
-            />
+          />
 
-            <Route
+          <Route
             exact
             path="/projects/:id/edit"
             render={props => {
               if (this.isAuthenticated()) {
-              return <ProjectEdit gitRepos={this.state.gitRepos} {...props} projects={this.projects} phases={this.state.phases} updateProject= {this.updateProject}   patchProject= {this.patchProject} />
-            } else {
-              return <Redirect to="/login" />;
-                }
+                return (
+                  <ProjectEdit
+                    gitRepos={this.state.gitRepos}
+                    {...props}
+                    projects={this.projects}
+                    phases={this.state.phases}
+                    updateProject={this.updateProject}
+                    patchProject={this.patchProject}
+                  />
+                );
+              } else {
+                return <Redirect to="/login" />;
+              }
             }}
           />
-           <Route
+          <Route
             exact
             path="/projects/:projectName/tasks"
             render={props => {
-              console.log(this.state.tasks)
-             let projectTask =''
-             if (this.isAuthenticated()) {
-               projectTask = this.state.tasks.filter(task =>
-                task.projectName === (props.match.params.projectName)
-                )
-           return(<Task {...props}  tasks={projectTask} projects={this.state.projects} phases={this.state.phases} deleteTask={this.deleteTask} updateProject={this.updateProject} updateTask ={this.updateTask} addTask ={this.addTask}/>)
-
-            } else {
-              return <Redirect to="/login" />;
-                }
+              console.log(this.state.tasks);
+              let projectTask = "";
+              if (this.isAuthenticated()) {
+                projectTask = this.state.tasks.filter(
+                  task => task.projectName === props.match.params.projectName
+                );
+                return (
+                  <Task
+                    {...props}
+                    tasks={projectTask}
+                    projects={this.state.projects}
+                    phases={this.state.phases}
+                    deleteTask={this.deleteTask}
+                    updateProject={this.updateProject}
+                    updateTask={this.updateTask}
+                    addTask={this.addTask}
+                  />
+                );
+              } else {
+                return <Redirect to="/login" />;
+              }
             }}
           />
 
-           <Route
-             exact
-             path="/tasks/:id/edit"
+          <Route
+            exact
+            path="/tasks/:id/edit"
             render={props => {
               if (this.isAuthenticated()) {
-               return <TaskEditCard {...props} tasks={this.state.tasks} phases={this.state.phases} projects={this.state.projects} updateTask={this.updateTask}  />
+                return (
+                  <TaskEditCard
+                    {...props}
+                    tasks={this.state.tasks}
+                    phases={this.state.phases}
+                    projects={this.state.projects}
+                    updateTask={this.updateTask}
+                  />
+                );
               } else {
                 return <Redirect to="/login" />;
-                  }
-             }}
-           />
-
-
+              }
+            }}
+          />
         </div>
       </React.Fragment>
-    )
-          }
+    );
+  }
+}
 
-      }
-
-      export default withRouter(ApplicationViews);
+export default withRouter(ApplicationViews);
